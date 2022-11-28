b23c4bbc15832485b5bd2bbb4405261ce8495133
JohnMagtoto <john.magtoto@vocovo.com>
2022-11-28 17:37:31 +0000

    Run bridge config script from docker-entrypoint

--- bridge-topic.sh
+++ bridge-topic.sh
@@ -6,12 +6,24 @@
 ### This is done by copying the contents of the topics file
 ### and inserting them in the generated bridge.conf file
 
+# Colourisation support for logging and output.
+function _colour() {
+  if [ "$MQ_INTERACTIVE" = "1" ]; then
+    # shellcheck disable=SC2086
+    printf '\033[1;31;'${1}'m%b\033[0m' "$2"
+  else
+    printf -- "%b" "$2"
+  fi
+}
+function red() { _colour "40" "$1"; }
+function blue() { _colour "34" "$1"; }
+
 ### 
 ### Accepted Arguments
 ###     $1 → Error Message
 ###
 function handleError() {
-    echo "Topic Configuration Script: ${1}" 1>&2
+    echo "[$(blue bridge-topic)] [$(red ERROR)] [$(date +'%Y%m%d-%H%M%S')] $1" >&2
     exit 1
 }
 
@@ -22,42 +34,32 @@ function handleError() {
 ###
 function setTopics() {
     ### Add remove everything between the '#topic' and the next config section
-    sed -e '/^#topic/,/^# .*/{/^#topic/!{/^# .*/!d;};}' bridge.conf >bridge.new.conf
+    sed -i -e '/^#topic/,/^# .*/{/^#topic/!{/^# .*/!d;};}' /mosquitto/config/sections/bridges.conf
 
     ### Insert the topics from the topics file
-    sed -i.tmp -e '/^#topic/ r '"${2}" bridge.new.conf
+    sed -i '/^#topic/ r '"${2}" /mosquitto/config/sections/bridges.conf
 
     ### Append a space after the last topic inserted
-    sed -i.tmp -e "\|^${1}|"'a \
+    sed -i "\|^${1}|"'a \
         
-        ' bridge.new.conf
+        ' /mosquitto/config/sections/bridges.conf
 }
 
-
-echo "${TOPIC_LIST_FILE}"
-if [ -z "${TOPIC_LIST_FILE}" ]; then
-    # echo "Topic file env variable not set!" 1>&2
-    # exit 1
+if [ -z "${TOPICS_FILE}" ]; then
     handleError "Topic file env variable not set!"
 fi
 
-if [ ! -f "${TOPIC_LIST_FILE}" ]; then
-    # echo "Topic file not found!" 1>&2
-    # exit 1
+if [ ! -f "${TOPICS_FILE}" ]; then
     handleError "Topic file not found!"
 fi
 
 ## Get the last topic from the topics file
-LAST_TOPIC=$(tail -1 "${TOPIC_LIST_FILE}")
-# @TODO: remove logging
-echo "${LAST_TOPIC}"
+LAST_TOPIC=$(tail -1 "${TOPICS_FILE}")
 
 if [ -z "${LAST_TOPIC}" ]; then
-    # echo "No topics found!" 1>&2
-    # exit 1
     handleError "No topics found!"
 fi
 
-setTopics LAST_TOPIC TOPIC_LIST_FILE
+setTopics "${LAST_TOPIC}" "${TOPICS_FILE}"
 
 exit 0
